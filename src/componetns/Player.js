f7ee0e8608172ef6747203f95862856622ce262b
Manoj Bhat <manudbhat@gmail.com>
2021-11-23 18:34:09 +0530

    the app can do play and pause, and we can drag the progress bar and set the time we wish

---
+++ src/componetns/Player.js
@@ -0,0 +1,109 @@
+import React,{useRef,useState} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay,
+        faAngleLeft,
+        faAngleRight,
+        faPause
+    } from '@fortawesome/free-solid-svg-icons'
+
+export default function Player(props) {
+    const {
+        setIsPlaying,
+        isPlaying,
+        currentSong,
+    } = props;
+
+    const [songInfo,setSongInfo] = useState({
+        currentTime:0,
+        duration:0
+    });
+    
+    function formatMinutes(time) 
+    {
+        return(
+            Math.floor(time/60) + ":" +("0" + Math.floor(time%60)).slice(-2)
+        )
+    }
+    const onTimeUpdateHandler = (e) =>{
+        const currentTime = e.target.currentTime;
+        const duration = e.target.duration;
+
+        setSongInfo({...songInfo,currentTime: currentTime,duration});
+    }
+    
+    const audioRef = useRef(songInfo.currentTime);
+    const handlePlay = () =>
+    { 
+        console.log(isPlaying)
+        if(isPlaying)
+        {
+            audioRef.current.pause(); 
+            setIsPlaying(!isPlaying);
+        }
+        else 
+        {
+            audioRef.current.play();
+            setIsPlaying(!isPlaying);
+        }
+
+        
+    };
+
+    const dragHandler = (e) => {
+        setSongInfo({...songInfo,currentTime:e.target.value});
+        audioRef.current.currentTime = e.target.value
+        console.log(e.target.value)
+    }
+    const handleSkipBack = () =>
+    {
+
+    }
+
+    const handleSkipForward = () =>
+    {
+                
+    }
+    return (
+        <div className="player">
+            <div className="time-control">
+                <p>{formatMinutes(songInfo.currentTime)}</p>
+                <input 
+                    type="range" 
+                    min = {0}
+                    max = {songInfo.duration}
+                    value = {songInfo.currentTime}
+                    onChange = {dragHandler}
+                />
+                <p>{formatMinutes(songInfo.duration)}</p>
+            </div>
+
+            <div className="play-control">
+                <FontAwesomeIcon 
+                    className= "skipbacck" 
+                    onClick={handleSkipBack} 
+                    size="2x" 
+                    icon={faAngleLeft}
+                />
+                <FontAwesomeIcon 
+                    className= "play" 
+                    onClick={handlePlay} 
+                    size="2x" 
+                    icon={isPlaying?faPause:faPlay}
+                />
+                <FontAwesomeIcon 
+                    className= "play" 
+                    onClick={handleSkipForward}
+                    size="2x" 
+                    icon={faAngleRight}
+                />
+            </div>
+            <audio 
+                ref={audioRef} 
+                src={currentSong.audio}
+                
+                onLoadedMetadata= {onTimeUpdateHandler}
+                onTimeUpdate= {onTimeUpdateHandler}
+            />
+        </div>
+    )
+}
